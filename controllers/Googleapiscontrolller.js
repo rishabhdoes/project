25f3c69886a146791c45d87dd5c33149f112d43b
rishabhdoes <rishab_861980@student.nitw.ac.in>
2023-05-26 16:07:47 +0530

    establishment type also added and strict bound applied on all

--- controllers/Googleapiscontrolller.js
+++ controllers/Googleapiscontrolller.js
@@ -8,11 +8,11 @@ const suggestionAutocomplete = async (req,res) => {
   try{
     const {city,text}=req.query;
     
-console.log(Coordinates[city][0]);
+//console.log(Coordinates[city][0]);
    
 var config1 = {
   method: "get",
-  url:`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&types=geocode&location=${Coordinates[city][0]},${Coordinates[city][1]}&radius=${Coordinates[city][2]}&key=${process.env.GMAP_API_KEY}`,
+  url:`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&types=geocode&location=${Coordinates[city][0]},${Coordinates[city][1]}&radius=${Coordinates[city][2]}&strictbounds=true&key=${process.env.GMAP_API_KEY}`,
   headers: {},
 };
   var config2 = {
@@ -20,14 +20,19 @@ var config1 = {
     url:`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&types=route|sublocality|street_address|postal_town|intersection&location=${Coordinates[city][0]},${Coordinates[city][1]}&radius=${Coordinates[city][2]}&strictbounds=true&key=${process.env.GMAP_API_KEY}`,
     headers: {},
   };
+  var config3 = {
+    method: "get",
+    url:`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&types=establishment&location=${Coordinates[city][0]},${Coordinates[city][1]}&radius=${Coordinates[city][2]}&strictbounds=true&key=${process.env.GMAP_API_KEY}`,
+    headers: {},
+  };
 
  
 
-  const response=await Promise.all([axios(config1),axios(config2)]);
+  const response=await Promise.all([axios(config1),axios(config2),axios(config3)]);
    
       const suggestions=response.map(res=>res.data.predictions.map((locationData)=>locationData.description));
       const allSuggestions=suggestions.flat();
-      res.status(200).json(allsuggestions);
+      res.status(200).json(allSuggestions);
 }
     catch(err){
       console.log(err);
@@ -62,7 +67,7 @@ const nearbyLocalities=async(req,res)=>{
  const coordinates=await getCoordinatesByLocation(text);
   var config1={
     method:"get",
-    url:`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates[0]},${coordinates[1]}&radius=10000&types=route&key=${process.env.GMAP_API_KEY}`,
+    url:`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates[0]},${coordinates[1]}&radius=10000&types=sublocality_level_3&key=${process.env.GMAP_API_KEY}`,
 
     headers: {},
   };
