8b845044cfb24ef86948a54bf55ebba11ff92fed
Aryan <27aryanraj@gmail.com>
2023-06-02 15:13:34 +0530

    favorites routes done

--- routes/secureRoutes.js
+++ routes/secureRoutes.js
@@ -18,7 +18,7 @@ router.use(userAuth);
 router.post("/newProperty/house/create", newHouseProperty);
 router.post("/newProperty/house/update/:houseId", updateHouseProperty);
 router.post("/newProperty/pg/create", newPgProperty);
-router.post("/newProperty/pg/update/:id", updatePgProperty);
+router.post("/newProperty/pg/update/:pgId", updatePgProperty);
 
 // fetch all user listings
 router.get("/user/mylistings/", getMyListings);
