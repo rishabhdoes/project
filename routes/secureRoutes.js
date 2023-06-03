439b68d82ff19be9d78052c902d5be46dbf46b10
rishabhdoes <rishab_861980@student.nitw.ac.in>
2023-06-03 23:54:08 +0530

    house validation

--- routes/secureRoutes.js
+++ routes/secureRoutes.js
@@ -8,6 +8,7 @@ const {
   updatePgProperty,
   getMyListings,
 } = require("../controllers/propertiesController");
+const { housesValidation } = require("../validators/auth");
 
 const router = Router();
 
@@ -15,13 +16,15 @@ router.use(userAuth);
 
 // map
 
-
 // properties
 router.post("/newProperty/house/create", newHouseProperty);
-router.post("/newProperty/house/update/:houseId", updateHouseProperty);
+router.post(
+  "/newProperty/house/update/:houseId",
+
+  updateHouseProperty
+);
 router.post("/newProperty/pg/create", newPgProperty);
 router.post("/newProperty/pg/update/:id", updatePgProperty);
-
 
 // fetch all user listings
 router.get("/user/mylistings/", getMyListings);
--- validators/auth.js
+++ validators/auth.js
@@ -1,6 +1,16 @@
 const { check } = require("express-validator");
 const db = require("../db");
 const { compare } = require("bcryptjs");
+const {
+  PropertyType,
+  BHKType,
+  PropertyAge,
+  PreferredTenants,
+  Furnishing,
+  Parking,
+} = require("../constants");
+const { Facing } = require("../constants");
+const { Coordinates } = require("../constants");
 
 const password = check("password")
   .isLength({ min: 6, max: 15 })
@@ -34,8 +44,6 @@ const emailExists = check("email").custom(async (value) => {
 
 // login validation
 const loginFieldsCheck = check("email").custom(async (value, { req }) => {
-
-  
   const user = await db.query("SELECT * from users WHERE email = $1", [value]);
   if (!user.rows.length) {
     throw new Error("Email does not exist");
@@ -47,26 +55,148 @@ const loginFieldsCheck = check("email").custom(async (value, { req }) => {
   );
 
   if (!validPassword) {
-
     throw new Error("Wrong credentials");
   }
-  
+
   req.user = user.rows[0];
 });
 
 const getUserFromEmail = check("email").custom(async (value, { req }) => {
   const user = await db.query("SELECT * from users WHERE email = $1", [value]);
   req.user = user.rows[0];
-})
+});
 const getUserFromId = check("id").custom(async (value, { req }) => {
-
   const user = await db.query("SELECT * from users WHERE id = $1", [value]);
   req.user = user.rows[0];
-})
+});
+
+const housesValidation = async (houses) => {
+  const {
+    partNo,
+    property_type,
+    bhk_type,
+    property_age,
+    built_up_area,
+    floor,
+    total_floor,
+    facing,
+    city,
+    locality,
+    landmark_street,
+    expected_rent,
+    rent_negotiable,
+    expected_deposit,
+    monthly_maintenance,
+    available_from,
+    preferred_tenants,
+    furnishing,
+    parking,
+    description,
+    bathroom,
+    balcony,
+    water_supply,
+    gym,
+    non_veg_allowed,
+    gated_security,
+  } = houses;
+
+  if (partNo == 1) {
+    if (!PropertyType.includes(property_type)) {
+      throw new Error("Invalid Property type");
+    }
+
+    if (!BHKType.includes(bhk_type)) {
+      throw new Error("Invalid BHK TYPE");
+    }
+
+    if (!PropertyAge.includes(property_age)) {
+      throw new Error("Invalid PropertyAge");
+    }
+    if (!built_up_area || built_up_area < 100) {
+      throw new Error("Invalid BuiltType Area");
+    }
+
+    if (!floor || floor < 0) {
+      throw new Error("Invalid Floor");
+    }
+
+    if (!total_floor || total_floor < 0 || total_floor < floor) {
+      throw new Error("Invalid Floor");
+    }
+    if (!facing || !Facing.includes(facing)) {
+      throw new Error("Invalid Facing");
+    }
+  }
+
+  if (partNo == 2) {
+    if (!city || !Object.keys(Coordinates).includes(city)) {
+      throw new Error("Invalid City");
+    }
+
+    await Coordinates.find(locality)
+      .then()
+      .catch((err) => {
+        throw new Error("Invalid  Locality ");
+      });
+
+    if (!landmark_street) {
+      throw new Error("Invalid Landmark");
+    }
+  }
+
+  if (partNo == 3) {
+    if (!expected_rent || expected_rent <= 0) {
+      throw new Error("Invalid expected rent");
+    }
+    if (
+      !rent_negotiable ||
+      !(rent_negotiable === "true" || rent_negotiable === "true")
+    ) {
+      throw new Error("Invalid rent_negotiable");
+    }
+    if (
+      !monthly_maintenance ||
+      !(
+        monthly_maintenance === "Maintenance Included" ||
+        monthly_maintenance === "Maintenance Extra"
+      )
+    ) {
+      throw new Error("Invalid monthly_maintenance");
+    }
+    if (!available_from) {
+      throw new Error("Invalid available_from Date");
+    }
+    if (!preferred_tenants || !PreferredTenants.includes(preferred_tenants)) {
+      throw new Error("Invalid preffered Tenants");
+    }
+
+    if (!furnishing || !Furnishing.includes(furnishing)) {
+      throw new Error("Invalid furnishing type");
+    }
+
+    if (!parking || Parking.includes(parking)) {
+      throw new Error("Invalid Parking type");
+    }
+  }
+
+  if (partNo == 4) {
+    if (!bathroom || bathroom < 1) {
+      throw new Error("Invalid bathroom data");
+    }
+
+    if (balcony && balcony < 0) {
+      throw new Error("Invalid balcony data");
+    }
+    if (!gated_security || !non_veg_allowed) {
+      throw new Error("Invalid gated_Security data");
+    }
+  }
+};
 
 module.exports = {
   registerValidation: [email, password, phoneNumber, emailExists],
   loginValidation: [email, loginFieldsCheck],
   emailValidation: [email, emailExists, getUserFromEmail],
-  tokenValidation: [password, getUserFromId]
+  tokenValidation: [password, getUserFromId],
+  housesValidation,
 };
