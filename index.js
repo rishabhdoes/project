878cf2bfa6b5fa728ee9707838f455c43763a8e5
Nikhil Pal <nikhilpal2468@gmail.com>
2023-06-10 02:47:40 +0530

    cors change

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ require("./middleware/passport-middleware");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: CLIENT_URL, credentials: true }));
+// app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
