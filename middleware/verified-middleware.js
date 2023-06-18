e4d6c93f1294b4c9797da6ba06688757656505a1
rishabhdoes <rishab_861980@student.nitw.ac.in>
2023-06-18 21:19:19 +0530

    admin search

--- middleware/verified-middleware.js
+++ middleware/verified-middleware.js
@@ -10,5 +10,15 @@ const checkUserVerified = async (req, res, next) => {
     next({ message: "not Verified User" });
   }
 };
+const checkUserBlocked = async (req, res, next) => {
+  const query1 = `SELECT verified from users WHERE id=$1`;
 
-module.exports = { checkUserVerified };
+  const { rows } = await db.query(query1, [req.user.id]);
+
+  if (rows && !rows[0]?.is_blocked) next();
+  else {
+    next({ message: "You are blocked for this activity" });
+  }
+};
+
+module.exports = { checkUserVerified, checkUserBlocked };
