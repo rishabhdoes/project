00140415805cba64c4ba165ec01f92e7deaea777
Nikhil Pal <nikhilpal2468@gmail.com>
2023-10-25 15:08:25 +0530

    adding enc_req in log

--- index.js
+++ index.js
@@ -120,6 +120,7 @@ app.post("/payment", async (request, response) => {
   // request.on("end", function () {
   if (data){
     encRequest = ccav.encrypt(body, keyBase64, ivBase64);
+    console.log("encRequest:", encRequest)
     formbody =
       '<form id="nonseamless" method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
       encRequest +
